70511ee243e45a80bca7da6732c5ecad8b09b5d9
ckormanyos <e_float@yahoo.com>
2014-04-06 20:52:15 +0200

    Initial commit.

---
+++ boost/math/special_functions/hypergeometric.hpp
@@ -0,0 +1,18 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright 2014 Not yet selected
+//  Copyright 2014 Christopher Kormanyos
+//  Copyright 2014 John Maddock
+//  Copyright 2014 Paul Bristow
+//  Distributed under the Boost
+//  Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef _BOOST_HYPERGEOMETRIC_2014_04_07_HPP_
+  #define _BOOST_HYPERGEOMETRIC_2014_04_07_HPP_
+
+  namespace boost { namespace math {
+
+  } } // namespace boost::math
+
+#endif // _BOOST_HYPERGEOMETRIC_2014_04_07_HPP_
