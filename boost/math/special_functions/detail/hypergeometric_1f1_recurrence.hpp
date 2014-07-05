ca685e073dd97e2b612a70cfd884de6ebdeb18c1
AntonBikineev <ant.bikineev@gmail.com>
2014-07-05 20:27:23 +0400

    Recurrence functions have been restructured, backward recurrence for b parameter has been added.

--- boost/math/special_functions/detail/hypergeometric_1f1_recurrence.hpp
+++ boost/math/special_functions/detail/hypergeometric_1f1_recurrence.hpp
@@ -15,30 +15,75 @@
 
   namespace boost { namespace math { namespace detail {
 
-  // forward declaration
-  template <class T, class Policy>
-  inline T hypergeometric_1f1_imp(const T& a, const T& b, const T& z, const Policy& pol);
-
-  template<class T>
-  inline T hypergeometric_1f1_recurrence_a(T&a, const T& b, const T& z, const T& last, T& first, T& second)
+  template <class T>
+  struct hypergeometric_1f1_recurrence_a_next_coefficients
   {
+    typedef std::pair<T, T> result_type;
+
+    hypergeometric_1f1_recurrence_a_next_coefficients(const T& a, const T& b, const T& z):
+      b(b), z(z), a(a)
+    {
+    }
+
+    result_type operator()()
+    {
+      const result_type result = std::make_pair<T, T>((a - b) / a, (((2 * a) - b) + z) / a);
+      --a;
+      return result;
+    }
+
+  private:
+    const T b, z;
+    T a;
+  };
+
+  template <class T>
+  struct hypergeometric_1f1_recurrence_b_next_coefficients
+  {
+    typedef std::pair<T, T> result_type;
+
+    hypergeometric_1f1_recurrence_b_next_coefficients(const T& a, const T& b, const T& z):
+      a(a), z(z), b(b)
+    {
+    }
+
+    result_type operator()()
+    {
+      const T cn = z * (b - a);
+      const result_type result = std::make_pair<T, T>((b * (b - 1)) / cn, (b * ((b - 1) + z)) / cn);
+      --b;
+      return result;
+    }
+
+  private:
+    const T a, z;
+    T b;
+  };
+
+  template <class T, class NextCoefs>
+  inline T hypergeometric_1f1_recurrence(NextCoefs& get_coefs, const T& last_index, T& first, T& second)
+  {
+    using std::swap;
+
     T third = 0;
 
     // probably we will need to change type of last and k from T to boost::uintmax_t
-    for (T k = 0; k < last; ++k)
+    for (T k = 0; k < last_index; ++k)
     {
-      const T an = (a - b) / a;
-      const T bn = (((2 * a) - b) + z) / a;
+      std::pair<T, T> next = get_coefs();
 
-      third = ((bn * second) - first) / an;
+      third = ((next.second * second) - first) / next.first;
 
-      --a;
       swap(first, second);
       swap(second, third);
     }
 
     return first;
   }
+
+  // forward declaration
+  template <class T, class Policy>
+  inline T hypergeometric_1f1_imp(const T& a, const T& b, const T& z, const Policy& pol);
 
   template <class T, class Policy>
   inline T hypergeometric_1f1_backward_recurrence_for_negative_a(const T& a, const T& b, const T& z, const Policy& pol)
@@ -54,8 +99,30 @@
     T second = detail::hypergeometric_1f1_imp(ak, b, z, pol);
 
     // probably we will need to change type of last and k from T to boost::uintmax_t
-    const T last = fabs(integer_part);
-    return detail::hypergeometric_1f1_recurrence_a(ak, b, z, last, first, second);
+    const T last_index = fabs(integer_part);
+    detail::hypergeometric_1f1_recurrence_a_next_coefficients<T> s(ak, b, z);
+
+    return detail::hypergeometric_1f1_recurrence(s, last_index, first, second);
+  }
+
+  template <class T, class Policy>
+  inline T hypergeometric_1f1_backward_recurrence_for_negative_b(const T& a, const T& b, const T& z, const Policy& pol)
+  {
+    BOOST_MATH_STD_USING // modf, fabs
+
+    T integer_part = 0;
+    T bk = modf(b, &integer_part);
+
+    // we will never get to infinite recursion here:
+    T first = detail::hypergeometric_1f1_imp(a, bk, z, pol);
+    --bk;
+    T second = detail::hypergeometric_1f1_imp(a, bk, z, pol);
+
+    // probably we will need to change type of last and k from T to boost::uintmax_t
+    const T last_index = fabs(integer_part);
+    detail::hypergeometric_1f1_recurrence_b_next_coefficients<T> s(a, bk, z);
+
+    return detail::hypergeometric_1f1_recurrence(s, last_index, first, second);
   }
 
   } } } // namespaces
